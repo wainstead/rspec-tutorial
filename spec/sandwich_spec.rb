6a83b18d03ba347ece5d7f1db0d6e69851556ae6
Stephen "Wainstead" Pawlowski <spawlowski@diligent.com>
2018-11-25 14:29:56 -0500

    Using a helper method instead of begin{}

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,17 +1,18 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-
-  before { @sandwich = Sandwich.new('delicious', []) }
-
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
+  end
+  
   it 'is delicious' do
-    taste = @sandwich.taste
+    taste = sandwich.taste
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'Cheese'
-    toppings = @sandwich.toppings
+    sandwich.toppings << 'Cheese'
+    toppings = sandwich.toppings
     expect(toppings).to_not be_empty
   end
 end
